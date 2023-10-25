f9459e9712697d0d917f79d07434043318d5136e
粘爲俊 <stu98058@gmail.com>
2023-10-25 15:37:03 +0800

    hello world

---
+++ 01057049-gittest.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    char* poopoo;
+    poopoo = "布恩色";
+    printf("%s\n",poopoo);//shit
+
+    printf("Hello World");
+
+    printf("已經成功push上github");
+
+
+
+return 0;
+}
